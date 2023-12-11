28d2f7f173412f83d1a7f68bf8a570a190616a49
geniuth <geniuth@gmail.com>
2023-12-11 21:54:37 +0900

    buf fix regen paddle, onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -224,6 +224,7 @@ protected:
   void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd);
   void drawHud(QPainter &p);
   void drawDriverState(QPainter &painter, const UIState *s);
+  void drawBrakeRegen(QPainter &painter);
   void paintEvent(QPaintEvent *event) override;
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
