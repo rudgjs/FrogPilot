a55fac70c76734bf0d5a091f13a55dfa24d86d87
geniuth <geniuth@gmail.com>
2024-12-08 01:16:37 +0900

    UI error

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -1363,7 +1363,7 @@ PedalIcons::PedalIcons(QWidget *parent) : QWidget(parent) {
 
 void PedalIcons::updateState(const UIScene &scene) {
   acceleration = scene.acceleration;
-  desiredAcceleration = scene.desired_acceleration;
+  desired_acceleration = scene.desired_acceleration;
   brakeLightOn = scene.brake_lights_on;
   dynamicPedals = scene.dynamic_pedals_on_ui;
   standstill = scene.standstill;
